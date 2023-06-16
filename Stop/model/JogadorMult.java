1a36f6b619d5deb7e00253eb0982b7d727692a0b
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-06-16 19:47:29 -0300

    Add files via upload

---
+++ Stop/model/JogadorMult.java
@@ -0,0 +1,11 @@
+package Stop.model;
+
+public class JogadorMult extends Jogador {
+	
+	public JogadorMult(String nome, String apelido, String nascimento, String sexo , int Tempo) {
+		super(nome, apelido, nascimento, sexo , Tempo);
+		// TODO Auto-generated constructor stub
+	}
+
+
+ }
