ac277d37875098b64266fb42ff7a0d5aef37ba82
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-06-16 19:38:55 -0300

    Update Jogador.java

--- src/Stop/model/Jogador.java
+++ src/Stop/model/Jogador.java
@@ -1,54 +1,53 @@
 package Stop.model;
 
-public class Jogador 
-{
-	private String Nome;
-	private String Apelido;
-	private String Nascimento;
-	private String Sexo;
+public class Jogador {
 	
-	public Jogador(String nome, String apelido, String nascimento, String sexo) 
-	{
+    private String nome;
+    private String apelido;
+    private String nascimento;
+    private String sexo;
+    private int tempo;
+    
+	public Jogador(String nome, String apelido, String nascimento, String sexo, int tempo) {
 		super();
-		Nome = nome;
-		Apelido = apelido;
-		Nascimento = nascimento;
-		Sexo = sexo;
-		
-	}
-	public String getNome() 
-	{
-		return Nome;
-	}
-	public void setNome(String nome) 
-	{
-		Nome = nome;
-	}
-	public String getApelido() 
-	{
-		return Apelido;
-	}
-	public void setApelido(String apelido) 
-	{
-		Apelido = apelido;
-	}
-	public String getNascimento() 
-	{
-		return Nascimento;
-	}
-	public void setNascimento(String nascimento) 
-	{
-		Nascimento = nascimento;
-	}
-	public String getSexo() 
-	{
-		return Sexo;
-	}
-	public void setSexo(String sexo) 
-	{
-		Sexo = sexo;
+		this.nome = nome;
+		this.apelido = apelido;
+		this.nascimento = nascimento;
+		this.sexo = sexo;
+		this.tempo = tempo;
 	}
 	
 	
-	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getApelido() {
+		return apelido;
+	}
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	public String getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public int getTempo() {
+		return tempo;
+	}
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+
+    
 }
