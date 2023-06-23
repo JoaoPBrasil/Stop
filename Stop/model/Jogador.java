49f7fa41067e318f77aa34705ebc2844a26f657b
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-06-23 19:29:02 -0300

    Update Jogador.java

--- Stop/model/Jogador.java
+++ Stop/model/Jogador.java
@@ -1,6 +1,7 @@
 package Stop.model;
 
-public class Jogador {
+public class Jogador 
+{
 	
     private String nome;
     private String apelido;
@@ -8,7 +9,8 @@ public class Jogador {
     private String sexo;
     private int tempo;
     
-	public Jogador(String nome, String apelido, String nascimento, String sexo, int tempo) {
+	public Jogador(String nome, String apelido, String nascimento, String sexo, int tempo) 
+	{
 		super();
 		this.nome = nome;
 		this.apelido = apelido;
@@ -18,34 +20,44 @@ public class Jogador {
 	}
 	
 	
-	public String getNome() {
+	public String getNome() 
+	{
 		return nome;
 	}
-	public void setNome(String nome) {
+	public void setNome(String nome) 
+	{
 		this.nome = nome;
 	}
-	public String getApelido() {
+	public String getApelido() 
+	{
 		return apelido;
 	}
-	public void setApelido(String apelido) {
+	public void setApelido(String apelido)
+	{
 		this.apelido = apelido;
 	}
-	public String getNascimento() {
+	public String getNascimento() 
+	{
 		return nascimento;
 	}
-	public void setNascimento(String nascimento) {
+	public void setNascimento(String nascimento) 
+	{
 		this.nascimento = nascimento;
 	}
-	public String getSexo() {
+	public String getSexo() 
+	{
 		return sexo;
 	}
-	public void setSexo(String sexo) {
+	public void setSexo(String sexo) 
+	{
 		this.sexo = sexo;
 	}
-	public int getTempo() {
+	public int getTempo() 
+	{
 		return tempo;
 	}
-	public void setTempo(int tempo) {
+	public void setTempo(int tempo) 
+	{
 		this.tempo = tempo;
 	}
 
